c9d8f0ca5b551303fa1565a7ec5a9a90528cdaed
Girish Chalamalasetti <your_email@example.com>
2026-01-23 02:46:54 -0600

    Run server.py from safe location, avoid /data volume issues

--- docker/server.py
+++ docker/server.py
@@ -1,6 +1,4 @@
 from http.server import SimpleHTTPRequestHandler, HTTPServer
-import os
-os.chdir("/data")
 server = HTTPServer(("0.0.0.0", 8080), SimpleHTTPRequestHandler)
 server.serve_forever()
 
